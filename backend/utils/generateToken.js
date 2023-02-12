221b8516314a2c352530a3fafe4b5d852a245f87
Caleb Mucheru <mucherucaleb7@gmail.com>
2023-02-12 20:36:01 +0300

    config exp

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
+    expiresIn: "30d",
   });
 };
 
